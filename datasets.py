8bef690fbd67c17c1cf4333442a9b5800802ed81
mharmanani <harma@MH-ZEPHYRUS-G15.localdomain>
2022-10-11 13:45:03 -0400

    add valid_loader

--- datasets.py
+++ datasets.py
@@ -1,9 +1,11 @@
 from __future__ import annotations
 import os
+import numpy as np
 import pandas as pd
 from torchvision.io import read_image
-from torch.utils.data import Dataset
-from torch.utils.data import DataLoader
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.sampler import SubsetRandomSampler
+
 
 from utils import generate_labels
 
@@ -35,9 +37,31 @@ class HistologyDataset(Dataset):
 # Create the labels file
 generate_labels(TRAIN_PATH, TEST_PATH)
 
+# Hyperparameters for batch GD and train/val split
+batch_size = 64
+validation_split = .2
+shuffle_dataset = True
+random_seed= 42
+
+# Creating data indices for training and validation splits:
+dataset_size = len(dataset)
+indices = list(range(dataset_size))
+split = int(np.floor(validation_split * dataset_size))
+if shuffle_dataset :
+    np.random.seed(random_seed)
+    np.random.shuffle(indices)
+train_indices, val_indices = indices[split:], indices[:split]
+
+# Creating PT data samplers and loaders:
+train_sampler = SubsetRandomSampler(train_indices)
+val_sampler = SubsetRandomSampler(val_indices)
+
 # Create the train data loader
 train_dataset = HistologyDataset(annotations_file=TRAIN_ANNOT, img_dir=TRAIN_PATH)
-train_loader = DataLoader(train_dataset)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler)
+
+# Create the validation data loader
+val_loader = DataLoader(train_dataset, batch_size=batch_size, sampler=val_sampler)
 
 # Create the test data loader
 test_dataset = HistologyDataset(annotations_file=TEST_ANNOT, img_dir=TEST_PATH)
