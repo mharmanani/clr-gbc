a347171376d3db0a3b4885808401c09fef1f041e
mharmanani <harma@MH-ZEPHYRUS-G15.localdomain>
2022-10-12 20:58:16 -0400

    add annotation gen for train folder

--- utils.py
+++ utils.py
@@ -4,6 +4,11 @@ import pandas as pd
 def generate_labels(train_path, test_path):
     val_annot = open('data/test_annot.csv', 'w')
     val_annot.write('img,label\n')
+    train_annot = open('data/train_annot.csv', 'w')
+    train_annot.write('img,label\n')
     for img in os.listdir(test_path):
         label = img[0:3]
         val_annot.write('{0}/{1},{2}\n'.format(test_path, img, label))
+    for img in os.listdir(train_path):
+        label = img[0:3]
+        train_annot.write('{0}/{1},{2}\n'.format(test_path, img, label))
