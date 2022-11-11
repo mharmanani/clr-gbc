fdbca8955aa6f5b75b20f88f09542a18c9d7cf14
mharmanani <harma@MH-ZEPHYRUS-G15.localdomain>
2022-11-11 03:16:31 -0500

    add baseline models

--- resnet.py
+++ resnet.py
@@ -1,10 +1,110 @@
 import os
-from turtle import down, forward
 import numpy as np
 import pandas as pd
 import torch
 from torch import nn
 from torchvision.io import read_image
+
+from utils import map_labels_to_int
+
+class ResNetWrapper(nn.Module):
+    def __init__(self, resnet, num_classes=2, device='cuda'):
+        super().__init__()
+        self.resnet = resnet
+        self.clf = nn.Sequential(
+            self.resnet,
+            nn.Linear(1000, 128),
+            nn.ReLU(),
+            nn.Linear(128, num_classes),
+            nn.Softmax(dim=1)
+        )
+
+        self.device = device
+
+    def forward(self, X):
+        return self.clf(X)
+
+    def finetune(self, train_loader, val_loader, learning_rate=1e-3, num_epochs=30):
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(self.clf.parameters(), lr=learning_rate)
+
+        # Arrays and variables for plotting metrics
+        train_acc, val_acc = 0, 0
+        epochs_arr = []
+        train_losses = []
+        train_accs = []
+        val_losses = []
+        val_accs = []
+
+        for epoch in range(1+num_epochs):
+            self.clf.train()
+            for batch in train_loader:
+                X, y = batch
+                y = map_labels_to_int(y, dtype='long')
+                X = X.to(self.device)
+                y = y.to(self.device)
+                optimizer.zero_grad()
+                z = self.clf(X)
+                train_loss = criterion(z, y)
+                train_loss.backward()
+                optimizer.step()
+
+                train_total = train_correct = 0
+                for idx, i in enumerate(z):
+                    if torch.argmax(i) == y[idx]:
+                        train_correct +=1
+                    train_total +=1
+                train_acc = train_correct / train_total
+
+            train_losses.append(train_loss.to('cpu').detach().numpy())
+            train_accs.append(train_acc)
+
+            self.clf.eval()
+            with torch.no_grad():
+                for batch in val_loader:
+                    X, y = batch
+                    y = map_labels_to_int(y, dtype='long')
+                    X = X.to(self.device)
+                    y = y.to(self.device)
+                    z = self.clf(X)
+                    val_loss = criterion(z, y)
+                    
+                    val_total = val_correct = 0
+                    for idx, i in enumerate(z):
+                        if torch.argmax(i) == y[idx]:
+                            val_correct +=1
+                        val_total +=1
+                    val_acc = val_correct / val_total
+
+                val_losses.append(val_loss.to('cpu').to('cpu').detach().numpy())
+                val_accs.append(val_acc)
+
+            epochs_arr.append(epoch)
+
+            print('[Epoch {0}] [train loss={1}] [train acc={2}] [val loss={3}] [val acc={4}]'
+                .format(epoch, train_loss, round(train_acc, 3), val_loss, round(val_acc, 3)))
+
+    def test(self, test_loader):
+        self.clf.eval()
+        with torch.no_grad():
+            total = correct = 0
+            for batch in test_loader:
+                X, y = batch
+                y = map_labels_to_int(y, dtype='long')
+                X = X.to(self.device)
+                y = y.to(self.device)
+                z = self.clf(X)
+                print(torch.argmax(z, axis=1))
+                print(y)
+                for idx, i in enumerate(z):
+                    if torch.argmax(i) == y[idx]:
+                        correct +=1
+                    total +=1
+            test_acc = correct / total
+
+        return test_acc
+
+        
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, downsample):
--- run_models.py
+++ run_models.py
@@ -1,26 +1,39 @@
 import argparse
 from byol import train_byol_model
-from torchvision.models.resnet import resnet50, resnet101, ResNet50_Weights
+import torch
+from torchvision.models.resnet import resnet18, resnet50, \
+    ResNet18_Weights, ResNet50_Weights
 from torch.optim import Adam
 from datasets import build_datasets
+from metrics import compute_test_accuracy
 
 # Import models
 from simclr import SimCLR
 from byol import train_byol_model
+from resnet import ResNetWrapper
+from cnn import LeNetClassifier
+from cnn import train as train_cnn
+from cnn import test as test_cnn
 
 def parse_option():
     parser = argparse.ArgumentParser('arguments for model training')
 
     parser.add_argument('--model', type=str, default='simclr',
                         help='model to train')
+    parser.add_argument('--mode', type=str, default='train',
+                        help='determine wether to train or test model')
     parser.add_argument('--batch_size', type=int, default=32,
                         help='batch size to train')
     parser.add_argument('--num_epochs', type=int, default=30,
                         help='epochs to train')
     parser.add_argument('--num_views', type=int, default=2,
                         help='epochs to train')
+    parser.add_argument('--baseline', type=str, default='resnet18',
+                        help='the model backbone to use')
     parser.add_argument('--pretrained', type=bool, default=True,
                         help='use pretrained weights')
+    parser.add_argument('--from_epoch', type=int, default=0,
+                        help='resume training by loading a checkpoint')
 
     opt = parser.parse_args()
 
@@ -33,21 +46,55 @@ def main():
 
     if opt.model == 'simclr':
         train_loader, val_loader, test_loader = build_datasets(batch_size=opt.batch_size, augment_views=True)
-        model = SimCLR(
+        simclr = SimCLR(
                     model_backbone=resnet50(weights=ResNet50_Weights.IMAGENET1K_V1 if opt.pretrained else None), 
                     batch_size=opt.batch_size,
                     num_epochs=opt.num_epochs,
                     num_views=opt.num_views,
                     device='cuda',
                 )
-        model.train(train_loader=train_loader, val_loader=val_loader)
-        model.test(test_loader=test_loader)
+        
+        if opt.from_epoch > 0:
+            simclr.model.load_state_dict(torch.load('checkpoints/simclr/{0}.pth'.format(opt.from_epoch)))
+
+        simclr.train(train_loader=train_loader, val_loader=val_loader)
+        train_loader, _, test_loader = build_datasets(batch_size=opt.batch_size, augment_views=False)
+        simclr.train_clf_head(train_loader=train_loader, num_epochs=100)#opt.num_epochs)
+        
+        if opt.mode == 'test':
+            if opt.from_epoch > 0:
+                simclr.model.load_state_dict(torch.load('checkpoints/simclr/{0}.pth'.format(opt.from_epoch)))
+            test_accuracy = simclr.test(test_loader=test_loader, batch_size=opt.batch_size)
+            print(test_accuracy)
 
     elif opt.model == 'byol':
-        train_byol_model(
+        byol = train_byol_model(
             model_backbone=resnet50(weights=ResNet50_Weights.IMAGENET1K_V1 if opt.pretrained else None), 
             epochs=opt.num_epochs,
             batch_size=opt.batch_size)
+
+    elif opt.model == 'baseline':
+        train_loader, val_loader, test_loader = build_datasets(batch_size=opt.batch_size, augment_views=False)
+        
+        if opt.baseline == 'resnet18':
+            resnet = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1 if opt.pretrained else None)
+            model = ResNetWrapper(resnet).to('cuda')
+            model.finetune(train_loader=train_loader, val_loader=val_loader)
+            test_accuracy = model.test(test_loader=test_loader)
+            print(test_accuracy)
+
+        elif opt.baseline == 'cnn':
+            model = LeNetClassifier()
+            train_cnn(model, train_loader, val_loader)
+            test_accuracy = test_cnn(model, test_loader=test_loader)
+            print(test_accuracy)
+        
+        elif opt.baseline == 'resnet50':
+            resnet = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1 if opt.pretrained else None) 
+            model = ResNetWrapper(resnet).to('cuda')
+            model.finetune(train_loader=train_loader, val_loader=val_loader)
+            test_accuracy = model.test(test_loader=test_loader)
+            print(test_accuracy)    
 
     else:
         print('None selected')
